c17fd840347cca5b26f2725ef672ac2653fa79fe
ps218180 <ps218180@edu.summacollege.nl>
2022-06-13 12:31:35 +0200

    connection + class + reference

---
+++ Project 3/Project 3/Classes/Verkiezingdb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project_3.Classes
+{
+    class Verkiezingdb
+    {
+        #region fields
+        MySqlConnection _connection = new MySqlConnection("Port=3309;Server=localhost;Database=;Uid=root;Pwd=;");
+        #endregion
+
+
+    }
+}
